8e19f62fd4be74153d0ced24160821557dae5cc6
Emmanuel ore <koladeore@gmail.com>
2025-01-13 13:34:59 +0100

    update-server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://task-manager-app-frontend-xxbs.vercel.app/"
+      "https://task-manager-app-frontend-xxbs.vercel.app"
     ],
     credentials: true,
   })
